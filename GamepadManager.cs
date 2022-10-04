70114578fd807fa895f66ac93c73ffa828d286de
joe <apjoey914@gmail.com>
2022-10-04 16:36:09 -0400

    Added optional inversion for key and gamepad events

--- GamepadManager.cs
+++ GamepadManager.cs
@@ -4,125 +4,108 @@ using Microsoft.Xna.Framework.Input;
 
 namespace MonoGameEvents;
 
-public class GamepadManager
-{ 
-  private readonly GamePadState[] prevState;
-  private readonly int debounce;
-  private int ptr = 0;
-  private readonly List<ButtonEvent> handles;
+public class GamepadManager {
+    private readonly GamePadState[] prevState;
+    private readonly int debounce;
+    private int ptr = 0;
+    private readonly List<ButtonEvent> handles;
 
-  private struct ButtonEvent
-  {
-    public Buttons button;
-    public PressType pressType;
-    public Action callback;
+    private struct ButtonEvent {
+        public Buttons button;
+        public PressType pressType;
+        public Action callback;
+        public bool invert;
 
-    public ButtonEvent(Buttons button, Action callback, PressType pressType)
-    {
-      this.button = button;
-      this.callback = callback;
-      this.pressType = pressType;
+        public ButtonEvent(Buttons button, Action callback, PressType pressType, bool invert) {
+            this.button = button;
+            this.callback = callback;
+            this.pressType = pressType;
+            this.invert = invert;
+        }
     }
-  }
 
-  private enum PressType
-  {
-    Pressed,
-    Held,
-    Released,
-  }
-
-  public GamepadManager(int debounce = 6)
-  {
-    this.debounce = debounce;
-    this.prevState = new GamePadState[debounce];
-    this.handles = new List<ButtonEvent>();
-  }
-
-  public void Update(GamePadState newState)
-  {
-    ++ptr;
-    ptr %= debounce;
-    prevState[ptr] = newState;
-
-    foreach (var handle in handles)
-    {
-      switch (handle.pressType)
-      {
-        case PressType.Held:
-          if (IsHeld(handle.button)) handle.callback();
-          break;
-        case PressType.Pressed:
-          if (IsPressed(handle.button)) handle.callback();
-          break;
-        case PressType.Released:
-          if (IsReleased(handle.button)) handle.callback();
-          break;
-      }
+    private enum PressType {
+        Pressed,
+        Held,
+        Released,
     }
-  }
 
-  private int FramesPressed(Buttons button)
-  {
-    var acc = 0;
-    for (var i = 0; i < debounce; i++)
-    {
-      if (prevState[i].IsButtonDown(button))
-      {
-        acc++; 
-      }
+    public GamepadManager(int debounce = 6) {
+        this.debounce = debounce;
+        this.prevState = new GamePadState[debounce];
+        this.handles = new List<ButtonEvent>();
     }
-    return acc;
-  }
 
-  private GamePadState GetState()
-  {
-    return GetState(0);
-  }
+    public void Update(GamePadState newState) {
+        ++ptr;
+        ptr %= debounce;
+        prevState[ptr] = newState;
 
-  private GamePadState GetState(int prev)
-  {
-    return prevState[(ptr - prev + debounce) % debounce];
-  }
-
-  private bool IsPressed(Buttons button)
-  {
-    return GetState().IsButtonDown(button) && FramesPressed(button) == 1;
-  }
-
-  private bool IsHeld(Buttons button, int threshold = 2)
-  {
-    if (GetState().IsButtonUp(button)) return false;
-    for (var i = 0; i < debounce; i++)
-    {
-      if (i >= threshold) return true;
-      if (GetState(i).IsButtonUp(button)) return false;
+        foreach (var handle in handles) {
+            switch (handle.pressType) {
+                case PressType.Held:
+                    if (IsHeld(handle.button) ^ handle.invert) handle.callback();
+                    break;
+                case PressType.Pressed:
+                    if (IsPressed(handle.button) ^ handle.invert) handle.callback();
+                    break;
+                case PressType.Released:
+                    if (IsReleased(handle.button) ^ handle.invert) handle.callback();
+                    break;
+            }
+        }
     }
-    return false;
-  }
 
-  private bool IsReleased(Buttons button)
-  {
-    return GetState().IsButtonUp(button) && GetState(1).IsButtonDown(button);
-  }
+    private int FramesPressed(Buttons button) {
+        var acc = 0;
+        for (var i = 0; i < debounce; i++) {
+            if (prevState[i].IsButtonDown(button)) {
+                acc++;
+            }
+        }
 
-  public void OnPressed(Buttons button, Action callback)
-  {
-    AddCallback(button, callback, PressType.Pressed);
-  }
+        return acc;
+    }
 
-  public void OnHeld(Buttons button, Action callback)
-  {
-    AddCallback(button, callback, PressType.Held);
-  }
+    private GamePadState GetState() {
+        return GetState(0);
+    }
 
-  public void OnReleased(Buttons button, Action callback)
-  {
-    AddCallback(button, callback, PressType.Released);
-  }
+    private GamePadState GetState(int prev) {
+        return prevState[(ptr - prev + debounce) % debounce];
+    }
 
-  private void AddCallback(Buttons button, Action callback, PressType pressType)
-  {
-    handles.Insert(0, new ButtonEvent(button, callback, pressType));
-  }
+    private bool IsPressed(Buttons button) {
+        return GetState().IsButtonDown(button) && FramesPressed(button) == 1;
+    }
+
+    private bool IsHeld(Buttons button, int threshold = 2) {
+        if (GetState().IsButtonUp(button)) return false;
+        for (var i = 0; i < debounce; i++) {
+            if (i >= threshold) return true;
+            if (GetState(i).IsButtonUp(button)) return false;
+        }
+
+        return false;
+    }
+
+    private bool IsReleased(Buttons button) {
+        return GetState().IsButtonUp(button) && GetState(1).IsButtonDown(button);
+    }
+
+    public void OnPressed(Buttons button, Action callback, bool invert = false) {
+        AddCallback(button, callback, PressType.Pressed, invert);
+    }
+
+    public void OnHeld(Buttons button, Action callback, bool invert = false) {
+        AddCallback(button, callback, PressType.Held, invert);
+    }
+
+    public void OnReleased(Buttons button, Action callback, bool invert = false) {
+        AddCallback(button, callback, PressType.Released, invert);
+    }
+
+    private void AddCallback(Buttons button, Action callback, PressType pressType, bool invert) {
+        handles.Insert(0, new ButtonEvent(button, callback, pressType, invert));
+    }
 }
